84871b367401b6bc72e5506e93b49654e9bf6db8
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-04-04 14:43:16 -0400

    Refactor search functionality in main.js

--- src/main.js
+++ src/main.js
@@ -37,51 +37,8 @@ app.route('/search')
     res.sendFile(path.join(__dirname, 'index.html'));
   })
   .post(async (req, res) => {
-    const title = req.body.title;
-    let fieldOffices = req.body.fieldOffices; // Store it in a variable
-    fieldOffices = fieldOffices.toLowerCase().replace(/\s/g, ''); // Update the variable
-    const race = req.body.race;
-    const weight = req.body.weight;
-    const age = req.body.age;
-    console.log("Form Data city:", fieldOffices); // Use the updated variable
-
-    // Construct the FBI API URL with the specified search parameters
-    const apiUrl = `https://api.fbi.gov/@wanted?pageSize=2000&page=1&sort_on=modified&sort_order=desc&title=${title}&field_offices=${fieldOffices}&weight=${weight}&age_min=${age}&age_max=${age}&race=${race}`;
-
-    console.log("Constructed API URL:", apiUrl);
-    console.log("Form Data2:", req.body.race); // Log the form data
-
-    try {
-      // Fetch data from the FBI API
-      const fetchData = await fetch(apiUrl, {
-        headers: {
-          'User-Agent': 'Your User Agent String', // Replace with your actual User-Agent
-        },
-      });
-
-      if (fetchData.ok) {
-        const gData = await fetchData.json();
-
-        // if (gData) {
-        //   console.log(gData.items);
-        // }
-
-        // Check if there are search results to render
-        if (gData && gData.items && gData.items.length > 0) {
-          // Render the results page with search results data
-          res.render('search-results', { results: gData.items });
-        } else {
-          // No search results found
-          res.status(404).send('No search results found.');
-        }
-      } else {
-        console.error(`Error fetching data: ${fetchData.status} - ${fetchData.statusText}`);
-        res.status(fetchData.status).json({ error: 'Failed to fetch data' });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+    res.send('Search results will be displayed here');
+    
   });
 
   app.get('*', (req, res) => {
