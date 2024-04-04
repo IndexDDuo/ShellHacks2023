ee315d7f1f39928043acabbbf9d659d69584afa0
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-04-04 04:52:22 -0400

    Update routes and file paths

--- src/main.js
+++ src/main.js
@@ -81,8 +81,7 @@ app.route('/search')
     }
   });
 
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
   
